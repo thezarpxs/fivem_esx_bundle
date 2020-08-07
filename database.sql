403974379af8f1d6496c74dc88a30f04c3a16c65
Henrik S Nielsen <1741035+henkall@users.noreply.github.com>
2020-08-07 12:44:21 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `FiveMESX`
+-- Database: `{MARIADB_DATABASE_NAME}`
 --
-CREATE DATABASE IF NOT EXISTS `FiveMESX` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-USE `FiveMESX`;
+CREATE DATABASE IF NOT EXISTS `{MARIADB_DATABASE_NAME}` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+USE `{MARIADB_DATABASE_NAME}`;
 
 -- --------------------------------------------------------
 
