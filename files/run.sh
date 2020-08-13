89bf8365faa6c2edd9a46c7a37c3016c9fd50b44
Henrik S Nielsen <1741035+henkall@users.noreply.github.com>
2020-08-13 21:21:16 +0200

    Update run.sh

--- files/run.sh
+++ files/run.sh
@@ -97,4 +97,4 @@ do
 	fi
 done
 
-exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@ > /dev/null 2>&1
